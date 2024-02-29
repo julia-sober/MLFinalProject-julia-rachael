78452dec08ab364affedce84ca4a4288bd39fdaf
Julia Sober <143358195+julia-sober@users.noreply.github.com>
2024-02-29 15:00:23 -0500

    Update finalProjectScript.py

--- finalProjectScript.py
+++ finalProjectScript.py
@@ -1,1 +1,17 @@
 # Here is our file
+# import modules
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# sklearn utilities
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+
+# sklearn models
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.decomposition import PCA
