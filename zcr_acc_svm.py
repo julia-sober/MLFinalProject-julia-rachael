d53b3852316ef14971b526d1ab2689b227c7e856
Julia Sober <sober.julia@gmail.com>
2024-03-23 21:19:35 -0400

    Work from earlier

--- zcr_acc_svm.py
+++ zcr_acc_svm.py
@@ -1,47 +1,66 @@
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import os
 import librosa
+import librosa.feature
+import librosa.display
+import IPython.display as ipd
 import numpy as np
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
 from scipy.io import wavfile
 from scipy import signal
-#from micromlgen import port
+from micromlgen import port
 
-def wav_to_ACC(folder_path):
+def wav_to_ACC_and_ZCR(folder_path):
     acc_list = []
+    #zcr_list = []
     # os.listdir gets all files in the specified directory
+    count = 0
     for filename in os.listdir(folder_path):
-        # Load the audio file (filename) - TODO: Look up what data_a is!
-        _, data_a = wavfile.read(filename)
-        # Convert data_a to a float
-        data_a = np.float32(data_a)
+        if count < 16:
+            full_path = os.path.join(folder_path, filename)
+            #plot_zcr(full_path)
+            x, sr = librosa.load(full_path)
+            #zcr = librosa.feature.zero_crossing_rate(x)
+            #zcr_list.append(zcr)
+            auto = sm.tsa.acf(x, nlags=2000)
+            acc_list.append(auto)
+            count += 1
 
-        corr = signal.correlate(data_a, data_a)
-        # TODO: Look up
-        lags = signal.correlation_lags(len(data_a), len(data_a))
+    acc_array = np.array(acc_list)
+    #print(acc_array.shape)
+    #zcr_array = np.array(zcr_list)
+    #print(zcr_array.shape)
 
-        corr = corr / np.max(corr)
+    #combined_array = np.concatenate((acc_array, zcr_array))
+    return acc_array
 
-        lag = lags[np.argmax(corr)]
-        print(lag, np.max(corr))
-        acc_list.append(lag, np.max(corr))
+def plot_zcr(full_path):
+    x, sr = librosa.load(full_path)
+    n0 = 6500
+    n1 = 7500
+    plt.figure(figsize=(14, 5))
+    plt.plot(x[n0:n1])
+    plt.show()
 
-def wav_to_ZCR(folder_path):
-    zcr_list = []
-    for filename in os.listdir(folder_path):
-        x, sr = librosa.load(filename)
-        zcr = librosa.feature.zero_crossing_rate(x, frame_length=2048, hop_length=512)
-        zcr_list.append(zcr)
 
-    return zcr_list
-
-light_rain = wav_to_ACC("/microphone-sampling/TestingSamples/lightShower")
-medium_rain = wav_to_ACC("/microphone-sampling/TestingSamples/mediumShower")
-heavy_rain = wav_to_ACC("/microphone-sampling/TestingSamples/heavyShower")
-heavy_couscous = wav_to_ACC("/microphone-sampling/TestingSamples/heavyCouscousHail")
-light_couscous = wav_to_ACC("/microphone-sampling/TestingSamples/lightCouscousHail")
-mason_rain = wav_to_ACC("/microphone-sampling/TestingSamples/MasonJarRain")
-nothing = wav_to_ACC("/microphone-sampling/TestingSamples/Nothing")
+light_rain = wav_to_ACC_and_ZCR("microphone-sampling/TestingSamples/lightShower/")
+print(light_rain.shape)
+medium_rain = wav_to_ACC_and_ZCR("microphone-sampling/TestingSamples/mediumShower/")
+print(medium_rain.shape)
+heavy_rain = wav_to_ACC_and_ZCR("microphone-sampling/TestingSamples/heavyShower/")
+print(heavy_rain.shape)
+heavy_couscous = wav_to_ACC_and_ZCR("microphone-sampling/TestingSamples/heavyCouscousHail/")
+print(heavy_couscous.shape)
+light_couscous = wav_to_ACC_and_ZCR("microphone-sampling/TestingSamples/lightCouscousHail/")
+print(light_couscous.shape)
+mason_rain = wav_to_ACC_and_ZCR("microphone-sampling/TestingSamples/MasonJarRain/")
+print(mason_rain.shape)
+nothing = wav_to_ACC_and_ZCR("microphone-sampling/TestingSamples/Nothing/")
+print(nothing.shape)
 
 # Create Labels for Light, Medium, Heavy
 light_rain_labels = np.full(len(light_rain),"light rain")
@@ -52,5 +71,7 @@ light_couscous_labels = np.full(len(light_couscous),"light couscous hail")
 mason_rain_labels = np.full(len(mason_rain),"mason jar rain")
 nothing_labels = np.full(len(nothing),"nothing")
 
-# Concatenate together the 10 NumPy arrays into one array
+# Concatenate together the 7 NumPy arrays into one array
 X = np.concatenate([light_rain,medium_rain,heavy_rain,heavy_couscous,light_couscous,mason_rain,nothing], axis=0)
+df = pd.DataFrame(X)
+print(df.head())
