b2bca7fcd6f319b530fabccf29c7b45f3b132776
Rachael Chertok <rachaelchertok@gmail.com>
2024-03-23 23:22:04 -0400

    I believe the number of rows and columns is correct. I removed the check for if count >16. The number of rows is based on the number of .wav files we have-so the number of audio samples we have. I don't think we want to limit that to 16 (will explain when we talk next). In addition, I tried to remove/not add .idea files. I learned that having someone else's .idea files can cause an error when trying to run code in PyCharm. (So if you do get an error when running, I may have accidentally left in some .idea files; just delete the folder and then it should run.

--- zcr_acc_svm.py
+++ zcr_acc_svm.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 from scipy.io import wavfile
 from scipy import signal
-from micromlgen import port
+#from micromlgen import port
 
 def wav_to_ACC_and_ZCR(folder_path):
     acc_list = []
@@ -20,15 +20,14 @@ def wav_to_ACC_and_ZCR(folder_path):
     # os.listdir gets all files in the specified directory
     count = 0
     for filename in os.listdir(folder_path):
-        if count < 16:
-            full_path = os.path.join(folder_path, filename)
-            #plot_zcr(full_path)
-            x, sr = librosa.load(full_path)
-            #zcr = librosa.feature.zero_crossing_rate(x)
-            #zcr_list.append(zcr)
-            auto = sm.tsa.acf(x, nlags=2000)
-            acc_list.append(auto)
-            count += 1
+        full_path = os.path.join(folder_path, filename)
+        #plot_zcr(full_path)
+        x, sr = librosa.load(full_path)
+        #zcr = librosa.feature.zero_crossing_rate(x)
+        #zcr_list.append(zcr)
+        auto = sm.tsa.acf(x, nlags=2000)
+        acc_list.append(auto)
+        count += 1
 
     acc_array = np.array(acc_list)
     #print(acc_array.shape)
@@ -46,32 +45,37 @@ def plot_zcr(full_path):
     plt.plot(x[n0:n1])
     plt.show()
 
+#light_rain, medium_rain, etc. will contain the acc_array (and in future the zcr_array as well, concatenated?), after the function returns
+#So, these will contain the features
+light_rain_features = wav_to_ACC_and_ZCR("microphone-sampling/TestingSamples/lightShower/")
+print(light_rain_features.shape)
+medium_rain_features = wav_to_ACC_and_ZCR("microphone-sampling/TestingSamples/mediumShower/")
+print(medium_rain_features.shape)
+heavy_rain_features = wav_to_ACC_and_ZCR("microphone-sampling/TestingSamples/heavyShower/")
+print(heavy_rain_features.shape)
+heavy_couscous_features = wav_to_ACC_and_ZCR("microphone-sampling/TestingSamples/heavyCouscousHail/")
+print(heavy_couscous_features.shape)
+light_couscous_features = wav_to_ACC_and_ZCR("microphone-sampling/TestingSamples/lightCouscousHail/")
+print(light_couscous_features.shape)
+mason_rain_features = wav_to_ACC_and_ZCR("microphone-sampling/TestingSamples/MasonJarRain/")
+print(mason_rain_features.shape)
+nothing_features = wav_to_ACC_and_ZCR("microphone-sampling/TestingSamples/Nothing/")
+print(nothing_features.shape)
 
-light_rain = wav_to_ACC_and_ZCR("microphone-sampling/TestingSamples/lightShower/")
-print(light_rain.shape)
-medium_rain = wav_to_ACC_and_ZCR("microphone-sampling/TestingSamples/mediumShower/")
-print(medium_rain.shape)
-heavy_rain = wav_to_ACC_and_ZCR("microphone-sampling/TestingSamples/heavyShower/")
-print(heavy_rain.shape)
-heavy_couscous = wav_to_ACC_and_ZCR("microphone-sampling/TestingSamples/heavyCouscousHail/")
-print(heavy_couscous.shape)
-light_couscous = wav_to_ACC_and_ZCR("microphone-sampling/TestingSamples/lightCouscousHail/")
-print(light_couscous.shape)
-mason_rain = wav_to_ACC_and_ZCR("microphone-sampling/TestingSamples/MasonJarRain/")
-print(mason_rain.shape)
-nothing = wav_to_ACC_and_ZCR("microphone-sampling/TestingSamples/Nothing/")
-print(nothing.shape)
+# Create Labels for Light, Medium, Heavy, etc.
+light_rain_labels = np.full(len(light_rain_features),"light rain")
+med_rain_labels = np.full(len(medium_rain_features),"medium rain")
+heavy_rain_labels = np.full(len(heavy_rain_features),"heavy rain")
+heavy_couscous_labels = np.full(len(heavy_couscous_features),"heavy couscous hail")
+light_couscous_labels = np.full(len(light_couscous_features),"light couscous hail")
+mason_rain_labels = np.full(len(mason_rain_features),"mason jar rain")
+nothing_labels = np.full(len(nothing_features),"nothing")
 
-# Create Labels for Light, Medium, Heavy
-light_rain_labels = np.full(len(light_rain),"light rain")
-med_rain_labels = np.full(len(medium_rain),"medium rain")
-heavy_rain_labels = np.full(len(heavy_rain),"heavy rain")
-heavy_couscous_labels = np.full(len(heavy_couscous),"heavy couscous hail")
-light_couscous_labels = np.full(len(light_couscous),"light couscous hail")
-mason_rain_labels = np.full(len(mason_rain),"mason jar rain")
-nothing_labels = np.full(len(nothing),"nothing")
-
-# Concatenate together the 7 NumPy arrays into one array
-X = np.concatenate([light_rain,medium_rain,heavy_rain,heavy_couscous,light_couscous,mason_rain,nothing], axis=0)
+# Concatenate together the 7 NumPy arrays into one array, which will be the features array
+X = np.concatenate([light_rain_features,medium_rain_features,heavy_rain_features,heavy_couscous_features,light_couscous_features,mason_rain_features,nothing_features], axis=0)
+# Concatenate together the 7 NumPy LABEL arrays into one array, which will be the target array
+Y = np.concatenate([light_rain_labels, med_rain_labels, heavy_rain_labels, heavy_couscous_labels, light_couscous_labels, mason_rain_labels, nothing_labels], axis=0)
 df = pd.DataFrame(X)
-print(df.head())
+df['target'] = Y
+print(df)
+
